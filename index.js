d667140044ffc72f3c4138a0a18cbe515ea95597
unknown <apurvjha123@gmail.com>
2023-10-16 23:32:33 +0530

    adding static build

--- index.js
+++ index.js
@@ -27,12 +27,10 @@ app.use('/api/v1',controller)
 app.use('/register',register)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
+  app.use(express.static(path.join(__dirname, 'build')));
+  
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
